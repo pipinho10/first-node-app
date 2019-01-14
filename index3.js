3d43710e67f82641be95766727bad9837971ba0d
pipinho <eltjon.sulaj@gmail.com>
2019-01-14 21:50:55 +0100

    Complete CRUD operations using MongoDB

---
+++ index3.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.log('Could not connect to MongoDB', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+    const course = new Course({
+        name: 'Angular Course',
+        author: 'Mosh',
+        tags: ['angular', 'frontend'],
+        isPublished: true
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+async function getCourses() {
+    const courses = await Course.find({
+        author: 'Mosh'
+    });
+    console.log(courses);
+}
+
+getCourses();
+//createCourse();
