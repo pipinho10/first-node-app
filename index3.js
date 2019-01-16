3d7439b132a59cfdd5fbf23cb373a15150968e02
esulaj <eltjon.sulaj@hightechict.nl>
2019-01-16 08:20:17 +0100

    Add schema validations

--- index3.js
+++ index3.js
@@ -5,11 +5,15 @@ mongoose.connect('mongodb://localhost/playground')
     .catch(err => console.log('Could not connect to MongoDB', err));
 
 const courseSchema = new mongoose.Schema({
-    name: String,
+    name: { type: String, required: true },
     author: String,
-    tags: [String],
-    date: {type: Date, default: Date.now},
-    isPublished: Boolean
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean,
+    price: {
+        type: Number,
+        required: function() { return this.isPublished; }
+    }
 });
 
 const Course = mongoose.model('Course', courseSchema);
@@ -19,11 +23,16 @@ async function createCourse() {
         name: 'Angular Course',
         author: 'Mosh',
         tags: ['angular', 'frontend'],
-        isPublished: true
+        isPublished: true,
+        price: 5
     });
 
-    const result = await course.save();
-    console.log(result);
+    try {
+        const result = await course.save();
+        console.log(result);
+    } catch (ex) {
+        console.log(ex.message);
+    }
 }
 
 async function getCourses() {
@@ -33,5 +42,5 @@ async function getCourses() {
     console.log(courses);
 }
 
-getCourses();
-//createCourse();
+// getCourses();
+createCourse();
